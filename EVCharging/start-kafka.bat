4b7973a40e58c29f13fe267421ca0759d3b268b9
jidv1-ua <jidv1@alu.ua.es>
2025-10-17 14:30:34 +0200

    .bat actualizado

--- EVCharging/start-kafka.bat
+++ EVCharging/start-kafka.bat
@@ -14,12 +14,12 @@ if %ERRORLEVEL% NEQ 0 (
 )
 
 echo.
-echo 2. Compilación exitosa
+echo 2. Compilacion exitosa
 echo 3. Ahora ejecuta en terminales separadas:
 echo.
 echo    Terminal 1: mvn exec:java -Dexec.mainClass="EV_Central" -Dexec.args="8081 localhost:9092"
 echo    Terminal 2: mvn exec:java -Dexec.mainClass="EV_CP_E" -Dexec.args="localhost:9092 CP-001 DRIVER-001"  
-echo    Terminal 3: mvn exec:java -Dexec.mainClass="EV_CP_M" -Dexec.args="localhost:9092 CP-001"
+echo    Terminal 3: mvn exec:java -Dexec.mainClass="EV_CP_M" -Dexec.args="localhost 8081 localhost:9092 CP-001"
 echo    Terminal 4: mvn exec:java -Dexec.mainClass="TestClient" -Dexec.args="localhost:9092 DRIVER-001"
 echo.
 pause
